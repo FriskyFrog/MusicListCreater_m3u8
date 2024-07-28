ba664d0b2e75b932a1da2de933d54e79284c893f
FriskyFrog <111581040+FriskyFrog@users.noreply.github.com>
2024-07-28 19:11:56 +0800

    Add files via upload

---
+++ MusicListCreater_m3u8.py
@@ -0,0 +1,93 @@
+import os
+
+def getInput():
+    #Show options.
+    #[0]=>Quit without alarming.
+    #[1]=>Generate files(processOption()).
+    #[2]=>Print usage then quit.
+    print("\
+        [0]Quit.(default)\n\
+        [1]Generate lists\n\
+        [2]Print usage and quit.\n")
+    #Get option.
+    user_input = input("Choose a option:")
+    return user_input
+
+def processOption():
+    #Target parent_dir.
+    parent_dir = input("Enter the targeted parent_dir:")
+    #Change dir.
+    if parent_dir != "":
+        os.chdir(parent_dir)
+    #Get content of parent_dir.
+    parent_list = os.listdir()
+    #Quit if empty.
+    if parent_list == []:
+        exit(1)
+
+    #Fetch names of sub_dir by iterating parent_dir.
+    for sub_list_name in parent_list:
+        #Neglect m3u8lists and scripts.
+        if ("py" in sub_list_name) or ("m3u8" in sub_list_name):
+            continue
+
+        #Create list for specific sub_dir.
+        #This must be done first, having the list file be created in parent_dir.
+        m3u8list = open("./%s.m3u8"%(sub_list_name), mode = "w")
+
+        #Change dir, ensuring that it is generating list for sub_dir.
+        os.chdir(sub_list_name)
+        #Fetch current dir.
+        sub_list_dir = os.getcwd()
+
+        #Get names of music then sort, forming a sequence of A~Z, making it neat.
+        sub_list = os.listdir()
+        sub_list.sort()
+
+        #Iterate each name in sub_dir.
+        for sub_list_file in sub_list:
+            #Neglect the lyric files (*.lrc).
+            if not (".mp3" in sub_list_file):
+                continue
+            #Add path to file.
+            m3u8list.write(sub_list_dir+"/"+sub_list_file+"\n")
+        #Save list.
+        m3u8list.close()
+        #Return to parent_dir, prepareing to make list for another sub_dir.
+        os.chdir("..")
+
+def chooseMethod(user_input):
+    if user_input == "1":
+        return processOption()
+    if user_input == "2":
+        #Usage attached here.
+        return lambda: print("Usage:\n\
+            ##Structure                ##Concept\n\
+            parent_dir             #parent_dir\n\
+            |___sub_dir01          The dir containing all the files.\n\
+                |___music01.mp3    Generated m3u8lists will be here,\n\
+                |___music02.mp3    named after names the sub_dir bellow.\n\
+                |___musicxx.mp3    #sub_dir\n\
+                |___...            Adequate classified ones.\n\
+            |___sub_dir02\n\
+                |___...            ##Notice\n\
+            |___sub_dirxx...       Files will be neglected,\n\
+                |___...            except *.mp3 and *.flac.\n\
+            |___sub_dir01.m3u8     Do not put files in the parent_dir,\n\
+            |___sub_dir02.m3u8     except *.py and *.m3u8,\n\
+            |___sub_dirxx.m3u8     while putting this script is recommended,\n\
+            |___...                for assigning parent_dir is no need. \n\
+            ")
+    #Quit if anything except "1" or "2" is entered.
+    else:
+        return lambda: exit(0)
+
+def main():
+    #Get input from console.
+    userInput = getInput()
+    #Match function then execute.
+    processingMethod = chooseMethod(userInput)
+    processingMethod()
+    exit(0)
+
+main()
